88e3e6d0000601ec154557819e20289d157d93b1
Vytautas Saltenis <vytas@rtfb.lt>
2020-10-04 19:25:59 +0300

    Placeholder for ExpressionStatement and String() methods
    Chapter 2.6.

--- ast/ast.go
+++ ast/ast.go
@@ -1,10 +1,15 @@
 package ast
 
-import "github.com/rtfb/tarsier/token"
+import (
+	"bytes"
+
+	"github.com/rtfb/tarsier/token"
+)
 
 // Node represents any node of the AST.
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 // Statement is the statement kind of the AST node: statements do not produce a
@@ -33,6 +38,15 @@ func (p *Program) TokenLiteral() string {
 	return p.Statements[0].TokenLiteral()
 }
 
+// String implements Node.
+func (p *Program) String() string {
+	var out bytes.Buffer
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
+}
+
 // LetStatement is the AST subtree containing a let statement.
 type LetStatement struct {
 	Token token.Token
@@ -46,6 +60,19 @@ func (ls *LetStatement) statementNode() {
 // TokenLiteral implements Node.
 func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
+}
+
+// String implements Node.
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+	out.WriteString(";")
+	return out.String()
 }
 
 // ReturnStatement is the AST subtree containing a return statement.
@@ -62,6 +89,39 @@ func (rs *ReturnStatement) TokenLiteral() string {
 	return rs.Token.Literal
 }
 
+// String implements Node.
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(rs.TokenLiteral() + " ")
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
+
+// ExpressionStatement is the AST subtree containing an expression.
+type ExpressionStatement struct {
+	Token      token.Token // the first token of the expression
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() {
+}
+
+// TokenLiteral implements Node.
+func (es *ExpressionStatement) TokenLiteral() string {
+	return es.Token.Literal
+}
+
+// String implements Node.
+func (es *ExpressionStatement) String() string {
+	if es.Expression == nil {
+		return ""
+	}
+	return es.Expression.String()
+}
+
 // Identifier is the AST subtree containing an identifier.
 type Identifier struct {
 	Token token.Token
@@ -74,4 +134,9 @@ func (i *Identifier) expressionNode() {
 // TokenLiteral implements Node.
 func (i *Identifier) TokenLiteral() string {
 	return i.Token.Literal
+}
+
+// String implements Node.
+func (i *Identifier) String() string {
+	return i.Value
 }
---
+++ ast/ast_test.go
@@ -0,0 +1,28 @@
+package ast
+
+import (
+	"testing"
+
+	"github.com/rtfb/tarsier/token"
+)
+
+func TestString(t *testing.T) {
+	program := &Program{
+		Statements: []Statement{
+			&LetStatement{
+				Token: token.Token{Type: token.Let, Literal: "let"},
+				Name: &Identifier{
+					Token: token.Token{Type: token.Ident, Literal: "myVar"},
+					Value: "myVar",
+				},
+				Value: &Identifier{
+					Token: token.Token{Type: token.Ident, Literal: "anotherVar"},
+					Value: "anotherVar",
+				},
+			},
+		},
+	}
+	if program.String() != "let myVar = anotherVar;" {
+		t.Errorf("program.String() wrong: got=%q", program.String())
+	}
+}
